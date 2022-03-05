c83231f13909b46f746037e6be45c2ff4e985f7d
sylcym <prusak.syl@gmail.com>
2022-03-05 16:03:31 +0100

    delete data from db

--- restaurants.js
+++ restaurants.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { request } from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 
@@ -6,37 +6,104 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost:27017/restaurants-db', {useNewUrlParser: true});
-
-const restaurantSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please check your data entry, no value specified!'],
-  },
-  manager: {
-    type: String,
-    required: [true, 'Please check your data entry, no value specified!'],
-  },
+mongoose.connect('mongodb://localhost:27017/restaurants-db', {
+  useNewUrlParser: true,
 });
 
-const RestaurantsChain = mongoose.model("RestaurantsChain", restaurantSchema);
+const restaurantSchema = new mongoose.Schema({
+  name: String,
+  manager: String,
+});
 
-app.route('/restaurantChains')
+const RestaurantsChain = mongoose.model('RestaurantsChain', restaurantSchema);
 
-.get(function (req, res) {
-  RestaurantsChain.find(function(err, chains){
-    if (!err) {
-      res.render('restaurantsChain', { 
-        sectionTitle: 'Restaurants Chains',
-        restaurantsChains: chains
-      })
-    } else {
-      res.render(err)
-    }
+app
+  .route('/restaurantChains')
+
+  .get(function (req, res) {
+    RestaurantsChain.find(function (err, chains) {
+      if (!err) {
+        res.render('restaurantsChain', {
+          sectionTitle: 'Restaurants Chains',
+          restaurantsChains: chains,
+        });
+      } else {
+        res.render(err);
+      }
+    });
   })
+
+  .post(function (req, res) {
+    const newChain = new RestaurantsChain({
+      name: req.body.name,
+      manager: req.body.manager,
+    });
+
+    newChain.save();
+
+    res.redirect('/restaurantChains');
+  });
+
+app.get('/restaurantChains/:chainId', function (req, res) {
+  RestaurantsChain.findOne({ _id: req.params.chainId }, function (err, chain) {
+    if (!err) {
+      res.render('chain', {
+        id: chain._id,
+        name: chain.name,
+        manager: chain.manager,
+      });
+    } else {
+      res.render(err);
+    }
+  });
+});
+
+app
+  .route('/restaurantChains/edit/:chainId')
+
+  .get(function (req, res) {
+    RestaurantsChain.findById(req.params.chainId, function (err, chain) {
+      if (!err) {
+        res.render('edit', {
+          sectionTitle: 'Edit chain',
+          id: chain._id,
+          name: chain.name,
+          manager: chain.manager,
+        });
+      } else {
+        res.render(err);
+      }
+    });
+  })
+
+  .put(function (req, res) {
+    console.log(req.params.chainId);
+    RestaurantsChain.findByIdAndUpdate(
+      req.params.chainId,
+      {
+        name: req.body.name,
+        manager: req.body.manager,
+      },
+      function (err) {
+        if (!err) {
+          res.redirect('/restaurantChains/:chainId');
+        } else {
+          res.render(err);
+        }
+      },
+    );
+  });
+app.get('/restaurantChains/delete/:chainId', function (req, res) {
+  RestaurantsChain.findByIdAndDelete(req.params.chainId, function (err) {
+    if (!err) {
+      res.redirect('/restaurantChains');
+    } else {
+      res.render(err);
+    }
+  });
 });
 
 app.listen(5050, function () {
