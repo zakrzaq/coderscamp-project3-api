3a84e7eddbe6574e42fae0cea157a2225d15b115
Agnieszka Urbanowicz <urbanowicz.uk@gmail.com>
2022-03-24 11:30:37 +0000

    add status codes to auth

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,15 +1,17 @@
 import jwt from "jsonwebtoken";
+import { httpStatus } from "../utils/httpStatusCode.js";
 
 export const verify = (req, res, next) => {
     const token = req.header("auth-token");
 
-    if (!token) return res.status(401).send("Unauthorized Access");
+    if (!token)
+        return res.status(httpStatus.FORBIDDEN).send("Unauthorized Access");
 
     try {
         const verified = jwt.verify(token, process.env.TOKEN_SECRET);
         req.user = verified;
         next();
     } catch (err) {
-        res.status(403).send("Forbidden response");
+        res.status(httpStatus.UNAUTHORIZED).send("Forbidden response");
     }
 };
