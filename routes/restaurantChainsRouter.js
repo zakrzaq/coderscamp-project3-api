257cabbdcdcb8dfac7c59feed5f123c1769fee01
sylcym <prusak.syl@gmail.com>
2022-04-22 20:15:17 +0200

    restaruant chain logo update

--- routes/restaurantChainsRouter.js
+++ routes/restaurantChainsRouter.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { restaurantsChainController } from '../controllers/restaurantsChainController.js';
-import { validateRestaurantChain, validateRestaurantChainId } from '../utils/restaurantChainsRouterValidation.js';
 import { uploadLogo } from '../controllers/multerLogoController.js';
 
 export const restaurantChainsRouter = express.Router();
@@ -8,22 +7,17 @@ restaurantChainsRouter
   .get('/', restaurantsChainController.getAllChains)
   .post(
     '/',
-    // validateRestaurantChain,
     uploadLogo,
     restaurantsChainController.addChain,
   )
   .get('/:id', 
-    // validateRestaurantChainId, 
     restaurantsChainController.getChainById)
   .delete(
     '/:id',
-    // validateRestaurantChainId,
     restaurantsChainController.deleteChainById,
   )
   .put(
     '/:id',
-    // validateRestaurantChainId,
-    // validateRestaurantChain,
     uploadLogo,
     restaurantsChainController.updateChainById,
   )
